f205765e7946aada45d22c890554a92fc97462ef
YoloEinhorn <154087239+YoloEinhorn@users.noreply.github.com>
2023-12-17 20:01:03 +0100

    fertig 1

---
+++ GitUI/CommandsDialogs/SettingsDialog/Plugins/PluginRootIntroductionPage.cs
@@ -0,0 +1,20 @@
+﻿using GitCommands;
+using GitUIPluginInterfaces;
+
+namespace GitUI.CommandsDialogs.SettingsDialog.Pages
+{
+    public partial class PluginRootIntroductionPage : SettingsPageBase
+    {
+        public PluginRootIntroductionPage(IServiceProvider serviceProvider)
+            : base(serviceProvider)
+        {
+            InitializeComponent();
+            InitializeComplete();
+        }
+
+        protected override SettingsSource GetCurrentSettings()
+        {
+            return AppSettings.SettingsContainer;
+        }
+    }
+}
---
+++ GitUI/HelperDialogs/FormEdit.cs
@@ -0,0 +1,20 @@
+﻿namespace GitUI.HelperDialogs
+{
+    public partial class FormEdit : GitModuleForm
+    {
+        public FormEdit(GitUICommands commands, string text)
+            : base(commands)
+        {
+            InitializeComponent();
+            InitializeComplete();
+            Viewer.InvokeAndForget(() => Viewer.ViewTextAsync("", text));
+            Viewer.IsReadOnly = false;
+        }
+
+        public bool IsReadOnly
+        {
+            get => Viewer.IsReadOnly;
+            set => Viewer.IsReadOnly = value;
+        }
+    }
+}
